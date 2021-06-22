8b197bfd8242352705f84bb59aecf2c3115c59fb
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-22 16:05:00 +0530

    update : added a console.error statement to console error when it occurs

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 const errorHandler = (err, req, res, next) => {
+    console.error("error occurred ", err.stack)
     res.status(500).json({ success: false, message: " Unexpected error occured", error: err.stack })
 }
 
